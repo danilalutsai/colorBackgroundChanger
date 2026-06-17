aa0f1dcd529d7a3b2aca90c483fd9f066fb8535d
Danila Lutsai <dlutsai98@gmail.com>
2026-06-17 10:20:28 +0300

    Javascript upgraded

--- js/app.js
+++ js/app.js
@@ -1,1 +1,13 @@
-console.log("Подключено")
+const button = document.getElementById('btn')
+
+const colors = ["#ff5733", "#f9ff33", "#3371ff", "red", "green", "rgb(85, 255, 51)"]
+
+button.addEventListener("click", () => {
+  console.log("click")
+})
+
+function getRandomNumber() {
+  console.log(Math.random())
+}
+
+getRandomNumber()
