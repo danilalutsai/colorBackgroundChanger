177a7f7e03cf1caec8cbebf87d0cea1e699874f8
Danila Lutsai <dlutsai98@gmail.com>
2026-06-17 16:41:21 +0300

    Javascript upgraded

--- js/app.js
+++ js/app.js
@@ -1,13 +1,43 @@
 const button = document.getElementById('btn')
+const color = document.querySelector(".color")
 
-const colors = ["#ff5733", "#f9ff33", "#3371ff", "red", "green", "rgb(85, 255, 51)"]
+
+const hex = [
+  'A',
+  'B',
+  'C',
+  'D',
+  'E',
+  'F',
+  '0',
+  '1',
+  '2',
+  '3',
+  '4',
+  '5',
+  '6',
+  '7',
+  '8',
+  '9',
+]
 
 button.addEventListener("click", () => {
-  console.log("click")
+  let hexColor = generateHex()
+  document.body.style.backgroundColor = hexColor
+  color.textContent = hexColor
 })
 
-function getRandomNumber() {
-  console.log(Math.random())
+function generateHex() {
+  let hexColor = "#"
+  for (let i = 0; i < 6; i++) {
+    hexColor += hex[getRandomNumber()]
+
+  }
+  
+  return hexColor
 }
 
-getRandomNumber()
+function getRandomNumber() {
+  return Math.floor(Math.random() * hex.length)
+}
+
